683a89d1ca41672991f91b5079de2aaaf6ea2530
Hilal BAYRAK <hilalbayrakx54@gmail.com>
2023-02-12 16:44:15 +0300

    Controllers added

---
+++ NLayer.API/Controllers/ProductsController.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : CustomBaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly IService<Product> _service;
+
+        public ProductsController(IService<Product> service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        //GET api/products
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var products = await _service.GetAllAsync();
+            var productsDTOs = _mapper.Map<List<ProductDTO>>(products.ToList());
+            //return Ok(CustomResponseDTO<List<ProductDTO>>.Success(200, productsDTOs));  
+            return CreateActionResult(CustomResponseDTO<List<ProductDTO>>.Success(200, productsDTOs));
+        }
+
+        //GET api/products/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _service.GetByIdAsync(id);
+            var productsDTO = _mapper.Map<ProductDTO>(product);
+            return CreateActionResult(CustomResponseDTO<ProductDTO>.Success(200, productsDTO));
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> Save(ProductDTO productDTO)
+        {
+            var product = await _service.AddAsync(_mapper.Map<Product>(productDTO));
+            var productsDTO = _mapper.Map<ProductDTO>(product);
+            return CreateActionResult(CustomResponseDTO<ProductDTO>.Success(201, productsDTO));
+        }
+
+        [HttpPut()]
+        public async Task<IActionResult> Update(ProductUpdateDTO productDTO)
+        {
+            await _service.UpdateAsync(_mapper.Map<Product>(productDTO));
+            return CreateActionResult(CustomResponseDTO<NoContentDTO>.Success(201));
+        }
+
+        //DELETE api/products/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var product = await _service.GetByIdAsync(id);
+            await _service.RemoveAsync(product);
+            var productsDTO = _mapper.Map<ProductDTO>(product);
+            return CreateActionResult(CustomResponseDTO<NoContentDTO>.Success(204));
+        }
+
+    }
+}
